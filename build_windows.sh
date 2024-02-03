ed8a774ce9643102af2b2b3e84c40a79478a9a41
xingguangcuican6666 <119158495+xingguangcuican6666@users.noreply.github.com>
2024-02-03 14:55:26 +0800

    更新 build_windows.sh

--- build_windows.sh
+++ build_windows.sh
@@ -1,1 +1,3 @@
 x86_64-w64-mingw32-g++ ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9
+i686-w64-mingw32-g++ ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9
+arm-none-eabi-gcc ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9 -march=armv7-a -mfloat-abi=hard -mfpu=neon
