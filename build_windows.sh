82c340bdf99ac3bebba0cdcf0e6c114e997e5a80
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2024-02-03 15:15:46 +0800

    fix build

--- build_windows.sh
+++ build_windows.sh
@@ -1,3 +1,3 @@
 x86_64-w64-mingw32-g++ ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9 -o apk_installer_Win_amd64.exe
 i686-w64-mingw32-g++ ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9 -o apk_installer_Win_i386.exe
-arm-none-eabi-gcc ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9 -march=armv7-a -mfloat-abi=hard -mfpu=neon -o apk_installer_Win_arm64.exe
+arm-none-eabi-gcc ./windows/$1 -static-libstdc++ -static-libgcc $2 $3 $4 $5 $6 $7 $8 $9 -o apk_installer_Win_arm64.exe
