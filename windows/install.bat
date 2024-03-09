40ef5f571ea08deff288febbd066186c9dabef8c
xingguangcuican6666 <119158495+xingguangcuican6666@users.noreply.github.com>
2024-03-09 08:02:40 +0000

    123

--- windows/install.bat
+++ windows/install.bat
@@ -3,4 +3,4 @@ set d=%cd%
 rename *.exe apk_installer.exe
 mkdir "%systemdrive%\Program Files\apk_installer\"
 robocopy /mir . "%systemdrive%\Program Files\apk_installer"
-setx path=%path%;"%systemdrive%\Program Files\apk_installer\"
+setx path "%path%;%systemdrive%\Program Files\apk_installer\"
