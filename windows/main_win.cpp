f84702b2f28273e5341e3f49293481d858579c3d
xingguangcuican6666 <119158495+xingguangcuican6666@users.noreply.github.com>
2024-03-09 07:16:18 +0000

    update config

--- windows/main_win.cpp
+++ windows/main_win.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <thread>
 #include <unistd.h>
+#include "../ini.h"
 //#include <bits/std_thread.h>
 //#define _GLIBCXX_HAS_GTHREADS
 using namespace std;
@@ -16,8 +17,9 @@ string input_s="";
 int port = 5555;
 string command = "";
 string temp = "";
+int read_ipconfig=0;
 int pdport = 5555;
-
+ini::iniReader config;
 int rs = 0;
 string ip ="";
 int input_i = 0;
@@ -47,6 +49,55 @@ int userinput_int(){
     };
 };
 int main(int argc, char *argv[]){
+    bool ret = config.ReadConfig("config.ini");
+    bool retf = config.ReadConfig("fconfig.ini");
+    if (retf == false) {
+        std::system("del /s /q fconfig.ini");
+        std::system("echo [main] >> fconfig.ini");
+        std::system("echo installer = 1 >> fconfig.ini");
+        std::cout << "这是你第一次打开软件,请选择你之后的使用方式:" << endl;
+        std::cout << "1.每次都手动[推荐Android 11及以上]" << endl;
+        std::cout << "2.自动使用配置文件(config.ini)[推荐Android 11以下]" << endl;
+        std::cout << "3.自行设置配置文件" << endl;
+        std::cout << "请选择:";
+        userinput_int();
+        if (input_i == 1) {
+            std::system("echo useconfig = 0 >> fconfig.ini");
+        }
+        else if (input_i == 2) {
+            std::system("echo useconfig = 1 >> fconfig.ini");
+            std::cout << "请输入ip:";
+            userinput_string();
+            system("echo [connect] >> config.ini");
+            command = "echo ip = "+input_s+" >> config.ini";
+            system(command.c_str());
+            std::cout << "请输入端口:";
+            userinput_int();
+            command = "echo port = "+to_string(input_i)+" >> config.ini";
+            std::system(command.c_str());
+            std::cout << "完成";
+            exit(0);
+        }
+        else if (input_i == 3) {
+            exit(0);
+        };
+        
+    };
+	if (retf == true) {
+        int usingconfig = config.ReadInt("main", "useconfig", 0);
+        if (usingconfig == 1) {
+            std::string HostName = config.ReadString("connect", "ip", "");
+	        int Port = config.ReadInt("connect", "port", 0);
+	        command = "adb connect "+HostName+":"+to_string(Port);
+            system(command.c_str());
+            temp = argv[1];
+            command = "adb install "+temp;
+            system(command.c_str());
+            return 0;
+            exit(0);
+        };
+    };
+
     _main:
     std::cout << "请输入安装模式(1.无线连接     2.有线连接):";
     userinput_int();
@@ -120,5 +171,4 @@ int main(int argc, char *argv[]){
         clear();
         exit(0);
     };
-
 }
